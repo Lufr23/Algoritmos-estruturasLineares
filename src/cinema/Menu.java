4d83e846da46bccb25adccdf47993a4c5b2e0222
Lufr23 <luiza.reis023@gmail.com>
2023-09-03 18:46:45 -0300

    Mensagem descritiva das alterações

--- src/cinema/Menu.java
+++ src/cinema/Menu.java
@@ -1,4 +1,17 @@
 package cinema;
+import java.lang.System;
+
 
 public class Menu {
+
+    public static void menu(){
+        System.out.println("============ MENU ===============");
+        System.out.println("1. Realizar reserva ");
+        System.out.println("2. Cancelar reserva ");
+        System.out.println("3. Escolher assentos ");
+        System.out.println("4. Mostrar quantidade pessoas na sessão ");
+
+
+    }
+
 }
