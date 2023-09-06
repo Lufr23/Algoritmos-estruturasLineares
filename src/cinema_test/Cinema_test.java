9891c987a7636ae2db105cd4de2765a2b9a8159f
natasha0600 <fernandeskelly0600@hotmail.com>
2023-09-05 21:18:01 -0300

    Adicionando a tela do cinema

--- src/cinema_test/Cinema_test.java
+++ src/cinema_test/Cinema_test.java
@@ -5,6 +5,7 @@ import cinema.Assento;
 public class Cinema_test {
     public static void main(String[] args) {
         Assento as1 = new Assento();
+        System.out.println("-----------TELA--------------");
         as1.imprimeAssento();
         as1.imprimeAssento2();
 
