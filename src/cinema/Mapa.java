7c161f48ba30acbb5ec2a9f502bb6e4a6947cba1
natasha0600 <fernandeskelly0600@hotmail.com>
2023-09-07 22:22:46 -0300

    adicionando novos métodos

--- src/cinema/Mapa.java
+++ src/cinema/Mapa.java
@@ -1,40 +1,89 @@
 package cinema;
 
+import javax.swing.*;
+
 public class Mapa {
-
- public static void imprimeMapa() {
-       int MAXIMO_ELEMENTOS = 14;
-       String[] identificador = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
-       String[] assentos = new String[MAXIMO_ELEMENTOS];
-
-       for (int c = 0; c < MAXIMO_ELEMENTOS; c++) {
-           assentos[c] = "[  ]"; // Preenche o array de colchetes com "[  ]"
-       }
-
-       for (int c = 0; c < identificador.length; c++) {
-           String elemento = identificador[c];
-           System.out.print(elemento + "  ");
-
-           for (int i = 0; i < MAXIMO_ELEMENTOS; i++) {
-               System.out.print(assentos[i] + " ");
-           }
-
-           System.out.println(); // Quebra de linha após cada fileira
-       }
+    int linha, coluna=0;
 
 
-   }
+    private static final int MAXIMO_ELEMENTOS = 14;
 
-   public static void tela(){
-       System.out.println("-------------------------------------------------------------------------");
-       System.out.println("|                               TELA                                    |");
-       System.out.println("-------------------------------------------------------------------------");
-       System.out.println();
-   }
 
-   public static void localizaAssento(){
-       System.out.println();
-       System.out.println("   [ 1] [ 2] [ 3] [ 4] [ 5] [ 6] [ 7] [ 8] [ 9] [10] [11] [12] [13] [14]");
+    public static void imprimeMapa() {
+        int MAXIMO_ELEMENTOS = 14;
+        String[] identificador = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+        String[] assentos = new String[MAXIMO_ELEMENTOS];
+        String[] resultado = new String [identificador.length+assentos.length];
+
+
+        for (int c = 0; c < MAXIMO_ELEMENTOS; c++) {
+            assentos[c] = "[  ]"; // Preenche o array de colchetes com "[  ]"
+        }
+
+        for (int c = 0; c < identificador.length; c++) {
+            String elemento = identificador[c];
+            System.out.print(elemento + "  ");
+
+            for (int i = 0; i < MAXIMO_ELEMENTOS; i++) {
+                System.out.print(assentos[i] + " ");
+            }
+
+            System.out.println(); // Quebra de linha após cada fileira
+        }
+
+
+
+
     }
+
+    public static void tela(){
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("|                               TELA                                    |");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println();
+    }
+
+    public static void localizaAssento(){
+        System.out.println();
+        System.out.println("   [ 1] [ 2] [ 3] [ 4] [ 5] [ 6] [ 7] [ 8] [ 9] [10] [11] [12] [13] [14]");
+    }
+
+    public boolean reservaAssento(String []resultado){
+        for(int i=0;i<resultado.length;i++){
+            if(resultado==null){
+                System.out.println("Há assentos vagos");return true;
+            }
+        }return false;
+    }
+    public void linhasEcolunas(){
+
+        if(linha<0 || linha > MAXIMO_ELEMENTOS){
+            System.out.println("\nLinha Inválida, digite outra linha válida\n");
+        }
+        else if (coluna<0 || coluna>12){
+            System.out.println("\n Coluna inválida, digite outra coluna válida!\n");
+
+        }
+        else{
+            System.out.println("batata");
+            {
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
